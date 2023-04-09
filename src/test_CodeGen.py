dc08428997123bfe1ae57d7f9d9dd10b00e1e6df
kaalam <kaalam@kaalam.ai>
2023-04-10 00:18:54 +0200

    [AUTOGEN] Included in copyright automation

--- src/test_CodeGen.py
+++ src/test_CodeGen.py
@@ -1,4 +1,4 @@
-# Jazz (c) 2018-2020 kaalam.ai (The Authors of Jazz), released as:
+# Jazz (c) 2018-2023 kaalam.ai (The Authors of Jazz), released as:
 #
 #  1. You can use this for research under a GPL-3 license
 #  2. See the main Jazz project or contact the authors (kaalam@kaalam.ai) for other licenses
